10b7d20a605ffd0c9a92fe520193ba1492f00ad3
Galiixy <gaelle_f@live.fr>
2020-06-26 12:53:02 +0200

    Research books on database fixed

--- app/src/main/java/com/example/dut_app_mobile/MainActivity.java
+++ app/src/main/java/com/example/dut_app_mobile/MainActivity.java
@@ -46,12 +46,11 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
-        /*else{
+        else{
             BDD bdd = new BDD(FirebaseFirestore.getInstance());
             bdd.getAllBooks();
             Log.d("Map books",bdd.getBooks().toString());
         }
-*/
 
 
 
