3543eb3f337c0a37cd2c07687a94676153c1dcb4
Noelle15 <noelle.verma152000@gmail.com>
2020-06-26 13:56:17 +0200

    nom menu à jour

--- app/src/main/java/com/example/dut_app_mobile/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/dut_app_mobile/ui/dashboard/DashboardFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.dut_app_mobile.R;
@@ -22,7 +19,7 @@ public class DashboardFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(R.layout.decouvrir, container, false);
         return root;
     }
 }
