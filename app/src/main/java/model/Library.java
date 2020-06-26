a9b7d0c4f5cc705523a6c2db4dc180ef79866964
Galiixy <gaelle_f@live.fr>
2020-06-26 10:51:53 +0200

    try to fix get all boooks

--- app/src/main/java/model/Library.java
+++ app/src/main/java/model/Library.java
@@ -5,18 +5,20 @@ package model;
  */
 public class Library {
     private Book book;
-    private int grade;
+    private long grade;
     private String comment;
+    private boolean bookmark;
 
     /**
      * @param book class
      * @param grade start to 1
      * @param comment
      */
-    public Library(Book book, int grade, String comment) {
+    public Library(Book book, long grade, String comment, Boolean bookmark) {
         this.book = book;
         this.grade = grade;
         this.comment = comment;
+        this.bookmark = bookmark;
     }
 
     /**
@@ -29,7 +31,7 @@ public class Library {
     /**
      * @return int grade, start to 1 finish to 5
      */
-    public int getGrade() {
+    public long getGrade() {
         return grade;
     }
 
